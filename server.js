2b7c826caa5a534006143efbde75c1a71fc1c888
tedkkraft <tedkkraft@gmail.com>
2020-09-17 12:00:03 -0400

    HTML routes added

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useUnifiedTopology: true 
 });
 
+app.use(require("./routes/html-routes"));
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
